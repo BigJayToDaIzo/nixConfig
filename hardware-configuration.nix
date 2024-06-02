b903dca82d2f7eecadf03474bca9a65f023b4111
Jay Emm <josephlmyers@gmail.com>
2024-06-02 12:08:43 -0500

    off vulkan_beta channel onto beta for nvidia

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -52,7 +52,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.vulkan_beta;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   fileSystems."/" =
