faeb6e0e27189f3598f8b0e9012d626a57f6a018
Jay Emm <josephlmyers@gmail.com>
2024-06-01 22:17:12 -0500

    added btop and pfetch packages to home manager

--- home.nix
+++ home.nix
@@ -6,10 +6,19 @@
   home.username = "jm";
   home.homeDirectory = "/home/jm";
 
+  home.packages = [
+    pkg.btop
+    pkg.pfetch
+  ];
+
   
 
   home.stateVersion = "23.11";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  # programs.program here and beyond
+
+  # services.service here and beyond
 }
