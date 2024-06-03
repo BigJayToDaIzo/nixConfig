4f5f4fdcdcb272a57d91e0d5d58397adf35d5d48
Jay Emm <josephlmyers@gmail.com>
2024-06-03 15:27:23 -0500

    properly install nerd fonts

--- home.nix
+++ home.nix
@@ -1,34 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  # Home manager needs a bit of information about you and the 
-  # paths it should manage.
-  home.username = "jm";
-  home.homeDirectory = "/home/jm";
-  home.packages = [
-  	pkgs.nerdfonts
-	pkgs.gh
-	pkgs.wl-clipboard
-	pkgs.wofi
-	pkgs.wofi-emoji
-	pkgs.btop
-	pkgs.pfetch-rs
-	pkgs.zellij
-	pkgs.kitty
-	pkgs.taskwarrior3
-	pkgs.vlc
-	pkgs.ripgrep
-  ];
-  home.stateVersion = "24.05";
+	# Home manager needs a bit of information about you and the 
+	# paths it should manage.
+	home.username = "jm";
+	home.homeDirectory = "/home/jm";
+	home.packages = [
+		pkgs.gh
+		pkgs.wl-clipboard
+		pkgs.wofi
+		pkgs.wofi-emoji
+		pkgs.btop
+		pkgs.pfetch-rs
+		pkgs.zellij
+		pkgs.kitty
+		pkgs.taskwarrior3
+		pkgs.vlc
+		pkgs.ripgrep
+	];
+	home.stateVersion = "24.05";
 
-  # programs.program here and beyond
-  # Let Home Manager install and manage itself.
-  programs = {
-  	home-manager.enable = true;
-	starship = {
-		enable = true;
+	fonts.packages = with pkgs; [
+		nerdfonts
+	];
+
+	# programs.program here and beyond
+	# Let Home Manager install and manage itself.
+	programs = {
+		home-manager.enable = true;
+		starship = {
+			enable = true;
+		};
 	};
-  };
-  # services.service here and beyond
+	# services.service here and beyond
 
 }
